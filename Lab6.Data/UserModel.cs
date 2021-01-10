20c26dc73a97c2956e2a8b6d497d76a8c4ea4e99
User <User@DESKTOP-HVULQQV>
2021-01-11 01:44:51 +0200

    Cleanup project

--- Lab6.Data/UserModel.cs
+++ Lab6.Data/UserModel.cs
@@ -1,27 +1,19 @@
-﻿using System.Runtime.Serialization;
-
+﻿
 namespace Lab6.Data
 {
-    [DataContract]
     public class UserModel
     {
-        [DataMember]
         public int Id { get; set; }
 
-        [DataMember]
         public string Name { get; set; }
 
-        [DataMember]
         public string Surname { get; set; }
 
-        [DataMember]
         public string Email { get; set; }
 
-        [DataMember]
         public UserType Type { get; set; }
 
         public UserModel() {
-
         }
 
         public UserModel(int id, string name, string surname, string email, UserType type)
@@ -38,12 +30,9 @@ namespace Lab6.Data
             return $"User {Id} - {Name} {Surname} - {Email}";
         }
 
-        [DataContract]
         public enum UserType
         {
-            [EnumMember]
             User,
-            [EnumMember]
             Manager
         }
     }
